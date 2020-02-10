2470506c44ad406b1d43266ba90855ef88871f53
Jeremy Dost <jdost@sharpdotinc.com>
2020-02-10 09:20:39 -0500

    update api url

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export default {
     },
     apiGateway: {
       REGION: "us-east-1",
-      URL: "https://p9gor2zuue.execute-api.us-east-1.amazonaws.com/dev"
+      URL: "https://70ay4jkc7a.execute-api.us-east-1.amazonaws.com/dev"
     },
     cognito: {
       REGION: "us-east-1",
